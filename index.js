f58050e8bc492097597916e7e58dea0200498f40
lujn21 <rodriguezlujandavid@gmail.com>
2024-06-12 15:32:05 -0500

    New

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import app from "./src/server.js";
+import app from "../src/server.js";
 
-app.listen(app.get("port"), () =>{
-    console.log(`Conectado al puerto ${app.get('port')}`);
-});
+export default async (req, res) => {
+  await new Promise(resolve => app.listen(app.get("port"), resolve));
+  console.log(`Conectado al puerto ${app.get('port')}`);
+  res.end();
+};
